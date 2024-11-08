7711b589228c6408964ef41cf44300d14da79f37
cebarboza <c.barbozagutierrez@erasmusmc.nl>
2024-11-08 11:45:30 +0100

    moving get empty cdm to patients cdm

--- R/generateTestTable.R
+++ R/generateTestTable.R
@@ -58,26 +58,3 @@ generateTestTables <- function(tableNames,
   # save the workbook
   openxlsx::saveWorkbook(wb, excelFileOutPath, overwrite = TRUE)
 }
-
-
-getEmptyCDM <- function(cdmName, cdmVersion) {
-
-  vocabPath <- file.path(Sys.getenv("EUNOMIA_DATA_FOLDER"),
-                         glue::glue("empty_cdm_{cdmVersion}.zip"))
-
-  if (!file.exists(vocabPath)) {
-    CDMConnector::downloadEunomiaData(datasetName = "empty_cdm",
-                                      cdmVersion = cdmVersion,
-                                      pathToData = Sys.getenv("EUNOMIA_DATA_FOLDER"),
-                                      overwrite = TRUE)
-  }
-
-  conn <- DBI::dbConnect(duckdb::duckdb(CDMConnector::eunomia_dir("empty_cdm")))
-  cdm <- CDMConnector::cdmFromCon(con = conn,
-                                  cdmSchema = "main",
-                                  writeSchema = "main",
-                                  cdmName = cdmName)
-
-  return(cdm)
-
-}
