d38ab35870707d46284ec1cdbc48b6b2487bd7a8
Cesar Barboza <ce.barboza@pm.me>
2025-02-18 11:50:21 +0100

    patients cdm

--- tests/testthat/test-patients.R
+++ tests/testthat/test-patients.R
@@ -1,4 +1,47 @@
-test_that("checkColumns function works", {
+test_that("checkTablesColumns function works csv with test data", {
+  filePath <- testthat::test_path("test_cdm_data.xlsx")
+  checkmate::assertCharacter(filePath)
+  checkmate::assertFileExists(filePath)
+  cdmVersion <- "5.3"
+  listPatientTables <- checkTablesColumns(cdmVersion, filePath, extraTable = FALSE)
+
+  expect_in(names(listPatientTables), c("person", "observation_period", "condition_occurrence", "pregnancy",
+                                        "measurement", "drug_exposure", "visit_detail", "visit_occurrence",
+                                        "procedure_occurrence", "device_exposure", "observation", "note",
+                                        "death", "note_nlp", "specimen", "fact_relationship"))
+})
+
+test_that("checkTablesColumns function works csv with table 'pregnancy'", {
+  filePath <- testthat::test_path("test_cdm_data_pregnancy.xlsx")
+  checkmate::assertCharacter(filePath)
+  checkmate::assertFileExists(filePath)
+  cdmVersion <- "5.3"
+  listPatientTables <- checkTablesColumns(cdmVersion, filePath, extraTable = TRUE)
+
+  expect_in(names(listPatientTables), c("person", "observation_period", "condition_occurrence", "pregnancy",
+                                        "measurement", "drug_exposure", "visit_detail", "visit_occurrence",
+                                        "procedure_occurrence", "device_exposure", "observation", "note",
+                                        "death", "note_nlp", "specimen", "fact_relationship"))
+})
+
+test_that("checkTablesColumns function works csv with table 'pregnancy'", {
+  filePath <- testthat::test_path("test_cdm_data_pregnancy.xlsx")
+  checkmate::assertCharacter(filePath)
+  checkmate::assertFileExists(filePath)
+  cdmVersion <- "5.3"
+
+  expect_error(checkTablesColumns(cdmVersion, filePath, extraTable = FALSE))
+})
+
+test_that("checkTablesColumns function works csv", {
+  filePath <- testthat::test_path("test_cdm_data_pregnancy.xlsx")
+  checkmate::assertCharacter(filePath)
+  checkmate::assertFileExists(filePath)
+  cdmVersion <- "5.3"
+  expect_error(checkTablesColumns(cdmVersion, filePath, extraTable = "pregnancys"))
+})
+
+test_that("fileColumnCheck function works", {
   filePath <- testthat::test_path("mimic_sample")
   cdmVersion <- "5.3"
   listPatientTables <- fileColumnCheck(filePath, cdmVersion)
@@ -11,20 +54,40 @@ test_that("checkColumns function works", {
                                            "visit_occurrence"))
 })
 
-test_that("Reading patients XLSX and JSON creation", {
+test_that("Reading patients XLSX and JSON creation for pregnancy", {
   filePath <- testthat::test_path("testPatientsRSV.xlsx")
   # outputPath <- file.path(tempdir(), "test1")
   # dir.create(outputPath)
 
   # outputPath explicitly NULL to create the testCases in the testthat folder
-  readPatients.xl(filePath = filePath, outputPath = NULL)
+  readPatients.xl(filePath = filePath, outputPath = NULL, extraTable = FALSE)
   expect_true(file.exists(file.path(testthat::test_path("testCases"), "test.json")))
 })
 
+test_that("Reading patients XLSX and JSON creation", {
+  filePath <- testthat::test_path("test_cdm_data_pregnancy.xlsx")
+  # outputPath <- file.path(tempdir(), "test1")
+  # dir.create(outputPath)
+
+  # outputPath explicitly NULL to create the testCases in the testthat folder
+  readPatients.xl(filePath = filePath, testName = "pregnancy", outputPath = NULL, extraTable = TRUE)
+  expect_true(file.exists(file.path(testthat::test_path("testCases"), "pregnancy.json")))
+})
+
 test_that("Patients to CDM xlsx function", {
-  filePath <- test_path("testPatientsRSV.xlsx")
+  filePath <- testthat::test_path("testPatientsRSV.xlsx")
   TestGenerator::readPatients.xl(filePath = filePath, outputPath = NULL)
-  cdm <- TestGenerator::patientsCDM(pathJson = NULL, testName = "test")
+  cdm <- TestGenerator::patientsCDM(pathJson = NULL, testName = "pregnancy")
+  expect_equal(class(cdm), "cdm_reference")
+  number_persons <- cdm[["person"]] %>% dplyr::pull(person_id)
+  expect_equal(length(number_persons), 20)
+  duckdb::duckdb_shutdown(duckdb::duckdb())
+})
+
+test_that("Patients to CDM xlsx function pregnancy extra table", {
+  filePath <- testthat::test_path("test_cdm_data_pregnancy.xlsx")
+  TestGenerator::readPatients.xl(filePath = filePath, testName = "pregnancy", outputPath = NULL, extraTable = TRUE)
+  cdm <- TestGenerator::patientsCDM(pathJson = NULL, testName = "pregnancy")
   expect_equal(class(cdm), "cdm_reference")
   number_persons <- cdm[["person"]] %>% dplyr::pull(person_id)
   expect_equal(length(number_persons), 20)
@@ -32,7 +95,7 @@ test_that("Patients to CDM xlsx function", {
 })
 
 test_that("Read patients empty tables xl", {
-  filePath <- test_path("test_cdm_data.xlsx")
+  filePath <- testthat::test_path("test_cdm_data.xlsx")
   TestGenerator::readPatients.xl(filePath = filePath, outputPath = NULL)
   cdm <- TestGenerator::patientsCDM(pathJson = NULL, testName = "test")
   expect_equal(class(cdm), "cdm_reference")
@@ -42,7 +105,7 @@ test_that("Read patients empty tables xl", {
 })
 
 test_that("Read patients empty xl", {
-  filePath <- test_path("test_cdm_data.xlsx")
+  filePath <- testthat::test_path("test_cdm_data.xlsx")
   TestGenerator::readPatients.xl(filePath = filePath, outputPath = NULL)
   cdm <- TestGenerator::patientsCDM(pathJson = NULL, testName = "test")
   expect_equal(class(cdm), "cdm_reference")
